3175fdba8265b1967d448a16d6353d6f85089bd6
Justin Mayhew <mayhew@live.ca>
2019-07-03 17:09:51 -0300

    Release 1.0.0

--- greenstalk.py
+++ greenstalk.py
@@ -1,7 +1,7 @@
 import socket
 from typing import Any, BinaryIO, Dict, Iterable, List, Optional, Tuple, Union
 
-__version__ = '0.5.2'
+__version__ = '1.0.0'
 
 Body = Union[bytes, str]
 Stats = Dict[str, Union[str, int]]
