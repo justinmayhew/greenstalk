d4f1c8f07fd93d010223e46d4f83addfa6a52377
Gabriel Linder <linder.gabriel@gmail.com>
2019-08-05 22:02:41 +0200

    Fix tests.
    Signed-off-by: Gabriel Linder <linder.gabriel@gmail.com>

--- tests.py
+++ tests.py
@@ -357,7 +357,7 @@ def test_delete_job_reserved_by_other(c: Client) -> None:
     with pytest.raises(NotFoundError):
         c.delete(job)
     time.sleep(1)
-    c.delete(job)
+    other.delete(job)
 
 
 @with_beanstalkd()
